299bdee4c7d65c30e119a58e55b5338ba7035722
Gargi Chaudhuri <39960842+gchaudhuri@users.noreply.github.com>
2024-03-26 14:25:33 -0500

    Add files via upload

--- lab_9/map_lab9.js
+++ lab_9/map_lab9.js
@@ -1,23 +1,13 @@
-var Stamen_TonerBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20,
-	ext: 'png'
+var OpenStreetMap_DE = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
 var mymap = L.map('map', {
     center: [43.09157730670122, -89.41174811804763],
     zoom: 7,
-    layers: Stamen_TonerBackground,
+    layers: OpenStreetMap_DE,
 });  
-
-var icon = L.icon({
-    iconUrl: 'img/icon.png',
-    iconSize: [30, 30],
-    iconAnchor: [0, 0],
-    popupAnchor: [15,10],
-});
 
 
 var migrationLayer = new L.migrationLayer({
